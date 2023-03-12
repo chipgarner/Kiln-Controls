70fe07e32d3406b33f44ae0ec245083177c4623b
James Kirikland Garner <chip@garnertotalenergy.com>
2023-03-12 19:38:48 +0800

    Rechart working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <ResponsiveContainer minWidth={310} minHeight={600} height="80%">
+            <LineChart
+                width={250}
+                data={data}
+                margin={{ top: 30, right: 50, left: 30, bottom: 30 }} >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" label={{ value: 'Time', position: 'bottom'}}/>
+                    <YAxis label={{ value: 'Temperature', angle: -90, position: 'insideLeft' }}/>
+                    <Tooltip />
+                    <Legend verticalAlign="top" height={36}/>
+                    <Line type="linear" dataKey="pv" stroke="#8884d8"
+                          activeDot={{ r: 8 }} />
+                    <Line type="linear" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+        </ResponsiveContainer>
+    );}
 
 export default App;
---
+++ src/streamData.js
@@ -0,0 +1,49 @@
+// streamData.js
+
+export const streamData = [
+    {
+        Ronaldo: 116,
+        Neymar: 17,
+        Messi: 37,
+    },
+    {
+        Ronaldo: 101,
+        Neymar: 11,
+        Messi: 54,
+    },
+    {
+        Ronaldo: 156,
+        Neymar: 166,
+        Messi: 141,
+    },
+    {
+        Ronaldo: 196,
+        Neymar: 160,
+        Messi: 63,
+    },
+    {
+        Ronaldo: 173,
+        Neymar: 167,
+        Messi: 94,
+    },
+    {
+        Ronaldo: 103,
+        Neymar: 194,
+        Messi: 188,
+    },
+    {
+        Ronaldo: 114,
+        Neymar: 105,
+        Messi: 40,
+    },
+    {
+        Ronaldo: 55,
+        Neymar: 12,
+        Messi: 49,
+    },
+    {
+        Ronaldo: 78,
+        Neymar: 29,
+        Messi: 90,
+    },
+];
