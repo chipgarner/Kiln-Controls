505924ec058b3a93946184187cf82a294a5b5bcd
James Kirikland Garner <chip@garnertotalenergy.com>
2023-03-12 20:27:23 +0800

    Chart is working with temp and heat, heat needs its own y axis

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import data from "./data";
+import data from "./kiln_data";
+import moment from 'moment';
 
 function App() {
     return (
@@ -10,13 +11,16 @@ function App() {
                 data={data}
                 margin={{ top: 30, right: 50, left: 30, bottom: 30 }} >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" label={{ value: 'Time', position: 'bottom'}}/>
+                    <XAxis dataKey="time_ms"
+                           label={{ value: 'Time', position: 'bottom'}}
+                           domain={["dataMin", "dataMax"]}
+                           tickFormatter = {(unixTime) => moment(unixTime).format('HH:mm:ss Do')}
+                           type="number"/>
                     <YAxis label={{ value: 'Temperature', angle: -90, position: 'insideLeft' }}/>
                     <Tooltip />
                     <Legend verticalAlign="top" height={36}/>
-                    <Line type="linear" dataKey="pv" stroke="#8884d8"
-                          activeDot={{ r: 8 }} />
-                    <Line type="linear" dataKey="uv" stroke="#82ca9d" />
+                    <Line type="linear" dataKey="heat_factor" stroke="#8884d8" />
+                    <Line type="linear" dataKey="temperature" stroke="#ff9999" />
             </LineChart>
         </ResponsiveContainer>
     );}
