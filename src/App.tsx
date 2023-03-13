119c3381189fbbf0ffdd3f93555fa253ff971477
James Kirikland Garner <chip@garnertotalenergy.com>
2023-03-13 15:09:27 +0800

    WOrks plotting temps and heat rate real time in react recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import MainChart from "./MainChart";
 import "./App.css";
+import data from "./kiln_data";
+
+const WS_URL = 'ws://127.0.0.1:8081/status';
 
 function App() {
+    const { sendJsonMessage, readyState } = useWebSocket(WS_URL, {
+        onOpen: () => {
+            console.log('WebSocket connection established.');
+        },
+        share: true,
+        filter: () => false,
+        retryOnError: true,
+        shouldReconnect: () => true,
+        onMessage: (event: WebSocketEventMap['message']) =>  processMessages(event)
+    });
+
+    useEffect(() => {
+        console.log('In useEffect');
+        if(readyState === ReadyState.OPEN) {
+            sendJsonMessage({});
+        }
+    }, [sendJsonMessage, readyState]);
+
+    const [tempData, setTempData] = useState<
+        { time_ms: number, temperature: number, heat_factor: number }[]  >([]);
+
+    const processMessages = (event: { data: string; }) => {
+        console.log(event.data.length);
+        if (event.data.length > 40) {
+            const response = JSON.parse(event.data);
+            console.log(response);
+            setTempData(tempData => [...tempData, ...response]);
+        }};
+
     return (
         <div className="App">
-            <MainChart />
+            {MainChart(tempData)}
         </div>
      );}
 
--- src/MainChart.tsx
+++ src/MainChart.tsx
@@ -1,16 +1,15 @@
 import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
-import data from "./kiln_data";
 import './MainChart.css';
 import moment from "moment/moment";
 import React from "react";
 
-function MainChart() {
+function MainChart(tempData: { time_ms: number; temperature: number; heat_factor: number; }[]) {
     return (
         <div  className="wrapper">
             <h3> Kiln Status </h3>
             <ResponsiveContainer width = "99%" aspect={2} >
                 <LineChart
-                    data={data}
+                    data={tempData}
                     margin={{ top: 30, right: 50, left: 30, bottom: 30 }} >
                     <CartesianGrid strokeDasharray="3 3" fill="white"/>
                     <XAxis dataKey="time_ms"
